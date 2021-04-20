f32f196564471438d5d247651d1171fc9ed28463
inamuu <8310973+inamuu@users.noreply.github.com>
2021-04-20 21:53:53 +0900

    Fix redundancy source code

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ sso_login_helper
 ===
 
 This is a login helper extension of chrome
+
+1. Open chrome browser
+2.  Click the extension functions
+3.  Enable developer mode
+4.  Click 'loade unpackaged extesion'
+5.  Choose this src directory
--- src/content.js
+++ src/content.js
@@ -1,1 +1,14 @@
-window.open('ログイン画面だよ');
+window.onload = function() {
+
+  var access_url = location.href
+
+  switch(true) {
+    case /wiki\.kazuma\.tokyo\/wp-login\.php/.test(access_url):
+      var message = 'kazuma.tokyoのログイン画面です'
+      break
+    case /inamuu\.com\/wp-login\.php/.test(access_url):
+      var message = 'inamuu.comのログイン画面です'
+    break
+  }
+  window.alert(message);
+}
